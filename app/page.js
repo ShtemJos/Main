daf9a04d2b1c6b2551abb2f217ed5b715fa49cf4
Petro Kuzmychev <johnnyfearmekramer@gmail.com>
2024-10-20 12:35:05 -0600

    WIP

--- app/page.js
+++ app/page.js
@@ -3,39 +3,39 @@
 import React, { useState } from "react";
 import Modal from "./modal";
 import Element from "./Element";
+import img1 from "../public/images/1.png";
+import img2 from "../public/images/44.png";
+import img3 from "../public/images/3.png";
+import img4 from "../public/images/4.png";
 
 const Page = () => {
   return (
-    <div className="max-w-full md:max-w-5xl max-h-max h-screen flex items-center justify-center mx-auto p-3 md:p-24 relative">
-      <div className="w-full h-full grid grid-cols-1 md:flex md:flex-wrap overflow-y-auto">
+    <div className="max-w-full mx-auto md:max-w-5xl max-h-screen h-full flex items-center justify-center">
+      <div className="w-full flex flex-col md:flex-row sm:flex-wrap gap-5">
         <Element
-          bgClass="bg-img1 bg-cover"
+          imageUrl={img1}
           Tittle="Lorem Ipsum"
           txt="Description about project"
         />
         <Element
-          bgClass="bg-img2 bg-contain bg-center"
+          imageUrl={img2}
           Tittle="LOGICOM"
           txt="Description about project"
         />
         <Element
-          bgClass="bg-img3 bg-cover"
-          Tittle={
-            <div className="relative md:right-5">
-              <div className="relative md:right-[22px]">Coral</div>
-              <div className="md:-m-3 md:p-0">Coalition</div>
-            </div>
-          }
+          imageUrl={img3}
+          Tittle="Coral"
+          txt="Description about project"
         />
         <Element
-          bgClass="bg-img4 bg-contain bg-center"
+          imageUrl={img4}
           Tittle="GOODBODI"
           txt="Description about project"
         />
-        <div className="m-auto md:absolute md:bottom-10 md:right-24">
-          <Modal />
-        </div>
       </div>
+      {/* <div className="m-auto absolute md:bottom-10 md:right-24">
+        <Modal />
+      </div> */}
     </div>
   );
 };
